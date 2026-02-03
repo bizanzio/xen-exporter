bizanzio/xen-exporter

commit 7fa33376970a941cb874fb528eb2d0a2ec5d057e

Container can run unprivileged and with only the files it needs
* Add user and group to restrict access to the host OS and from changing the script
* Only include the files required
* Set unchanging values (like EXPOSE and STOPSIGNAL) at the top of the file to inprove caching.
* Put USER at the end of the file, after COPY statements to ensure permissions are handled properly.

Dockerfile
@@ -1,10 +1,22 @@
 FROM python:3.10-alpine
 
-COPY . /app
-
-RUN python3 -m pip install -r /app/requirements.txt
-
+# Setup the port to expose
 EXPOSE 9100/tcp
 
+# How to stop the container
 STOPSIGNAL SIGINT
+
+# Setup the running user
+RUN addgroup --gid 10001 xenexporter && \
+    adduser --uid 10001 xenexporter --gid 1001
+
+# Install dependencies
+COPY --chown=0:0 --chmod=0644 ./requirements.txt /app/requirements.txt
+RUN python3 -m pip install -r /app/requirements.txt
+
+# Install the actual script
+COPY --chown=0:0 --chmod=0644 ./xen-exporter.py /app/xen-exporter.py
+
+# How to start the script
+USER 10001
 ENTRYPOINT [ "python3", "/app/xen-exporter.py" ]

commit 52dc1d6c40fe084910073693076782609cbe6fe9

Fix GID typo in Dockerfile user creation
The user was being added to GID 1001 instead of GID 10001, which is
the GID of the xenexporter group created on the previous line.
This was a typo introduced in commit 7fa3337.

Dockerfile
@@ -8,7 +8,7 @@ STOPSIGNAL SIGINT
 
 # Setup the running user
 RUN addgroup --gid 10001 xenexporter && \
-    adduser --uid 10001 xenexporter --gid 1001
+    adduser --uid 10001 xenexporter --gid 10001
 
 # Install dependencies
 COPY --chown=0:0 --chmod=0644 ./requirements.txt /app/requirements.txt